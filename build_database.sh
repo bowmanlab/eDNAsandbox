2b70f524b4d7211258c31210be38d3dc08f04a30
Jeff Bowman <jsbowman@ucsd.edu>
2022-09-27 21:32:10 -0700

    Update build_database.sh
    Create loop for multiple genes

--- build_database.sh
+++ build_database.sh
@@ -2,4 +2,6 @@
 
 #python3 parse_midori.py
 
-clustalo --auto -i MIDORI2_UNIQ_NUC_GB251_CO1_RAW.select.fasta -o MIDORI2_UNIQ_NUC_GB251_CO1_RAW.select.align.fasta
+for GENE in CO1 CytB lrRNA srRNA;do
+	clustalo --auto --force -i MIDORI2_UNIQ_NUC_GB251_CO1_RAW.select.fasta -o MIDORI2_UNIQ_NUC_GB251_CO1_RAW.select.align.fasta
+done
