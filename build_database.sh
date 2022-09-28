87394b0398b1c7ad79624783448958d3c4fea8b1
Jeff Bowman <jsbowman@ucsd.edu>
2022-09-27 21:22:01 -0700

    Added shell script to control workflow, select fasta files ready for alignment

---
+++ build_database.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#python3 parse_midori.py
+
+clustalo --auto -i MIDORI2_UNIQ_NUC_GB251_CO1_RAW.select.fasta -o MIDORI2_UNIQ_NUC_GB251_CO1_RAW.select.align.fasta
--- parse_midori.py
+++ parse_midori.py
@@ -9,6 +9,9 @@ Created on Tue Sep 27 12:40:51 2022
 ## Data downloaded manually from http://www.reference-midori.info/download.php#
 ## Used RAW -> uniq data. Probably actually want RAW_sp -> uniq
 
+## Scripted download possible from direct links like:
+## wget http://www.reference-midori.info/download/Databases/GenBank250/SPINGO_sp/uniq/MIDORI2_UNIQ_SP_NUC_GB250_ND4L_SPINGO.fasta.gz
+
 from Bio import SeqIO
 import gzip
 import pandas as pd
@@ -38,4 +41,29 @@ for gene in genes:
             genes_present.loc[record_genome, gene] = 1
             print(gene, record_genome)
             
-genes_present.sum(axis = 1)
+## Which genomes have CO1, CytB, lrRNA, srRNA
+            
+genes_present_select = genes_present[~genes_present['CO1'].isnull()]
+genes_present_select = genes_present_select[~genes_present_select['Cytb'].isnull()]
+genes_present_select = genes_present_select[~genes_present_select['lrRNA'].isnull()]
+genes_present_select = genes_present_select[~genes_present_select['srRNA'].isnull()]
+
+## If you want a random subset for testing, do that here.
+
+genes_present_select = genes_present_select.sample(1000)
+
+## Iterate across input files again and identify those with all target genes.
+            
+target_genomes = set(genes_present_select.index)
+
+for gene in ['CO1', 'CytB', 'lrRNA', 'srRNA']:
+    found_records = []
+    with gzip.open('MIDORI2_UNIQ_NUC_GB251_' + gene + '_RAW.fasta.gz', 'rt') as gzfile:
+        for record in SeqIO.parse(gzfile, 'fasta'):
+            record_genome = str(record.id).split('.')[0]
+            if record_genome in target_genomes:
+                found_records.append(record)
+                print(gene, record_genome)
+    with open('MIDORI2_UNIQ_NUC_GB251_' + gene + '_RAW.select.fasta', 'w') as fasta_out:
+        SeqIO.write(found_records, fasta_out, 'fasta')
+                
